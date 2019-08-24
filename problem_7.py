367b5be611928b4807e8efd5242bedc518794ae0
Nagarajan CK <nagainvertibrate@gmail.com>
2019-08-24 16:21:42 +0530

    handle the route

--- problem_7.py
+++ problem_7.py
@@ -1,39 +1,80 @@
-# A RouteTrie will store our routes and their associated handlers
-class RouteTrie: #THE ENTIRE TRIE
+class RouteTrie:
     def __init__(self):
-        # Initialize the trie with an root node and a handler, this is the root path or home page node
         self.root = RouteTrieNode()
 
-    def insert(self, url,handler):
-        # Similar to our previous example you will want to recursively add nodes
-        # Make sure you assign the handler to only the leaf (deepest) node of this path
-        urlArr = url.split('/')
-        current_node = self.children
+
+    def insert(self, urlArr,handler):
+        current_node = self.root
         for i in urlArr:
             if(i not in current_node.children):
                 current_node.children[i]=RouteTrieNode()
             current_node = current_node.children[i]
+
         current_node.handler = handler
 
-    def find(self, url):
-        # Starting at the root, navigate the Trie to find a match for this path
-        # Return the handler for a match, or None for no match
-        urlArr = url.split('/')
+
+    def find(self, urlArr):
         current_node = self.root
         for i in urlArr:
             if(i not in current_node.children):
-                return None
+                return 'not found handler'
             current_node = current_node.children[i]
+
         return current_node.handler
 
 
-# A RouteTrieNode will be similar to our autocomplete TrieNode... with one additional element, a handler.
-class RouteTrieNode: #EACH INDIVIDUAL NODE
+
+class RouteTrieNode:
     def __init__(self):
-        # Initialize the node with children as before, plus a handler
         self.children={}
-        self.handler=None
+        self.handler='not found handler'
+
 
     def insert(self, endpoint):
-        # Insert the node as before
         self.children[endpoint] = RouteTrieNode()
+
+
+
+class Router:
+    def __init__(self,handler):
+        self.route = RouteTrie()
+        self.route.insert('/',handler)
+
+
+    def add_handler(self, path, handler):
+        if(path.endswith('/')):
+            urlArr=self.split_path(path[:-1])
+        else:
+            urlArr=self.split_path(path)
+        self.route.insert(urlArr,handler)
+
+
+    def lookup(self, path):
+        if(path.endswith('/')):
+            urlArr=self.split_path(path[:-1])
+        else:
+            urlArr=self.split_path(path)
+        return self.route.find(urlArr)
+
+
+    def split_path(self, path):
+        urlArr= path.split('/')
+        urlArr[0]='/'
+        return urlArr
+
+
+
+
+router=Router("root handler")
+router.add_handler("/home/about", "about handler")
+router.add_handler("/home/info/", "info handler")
+
+print(router.lookup("/")) # prints 'root handler'
+
+print(router.lookup("/home")) # prints 'not found handler' since it is not handled
+
+print(router.lookup("/home/about")) # prints 'about handler' without trailing slash
+
+print(router.lookup("/home/about/")) # prints 'about handler' with trailing slash
+
+print(router.lookup("/home/info"))  # prints 'info handler'
